c2c4d148d584d2f09c9da27d3f433a3e5edbf9d7
Rude Code <abdullah.noman083@gmail.com>
2020-11-08 17:38:35 +0600

    noman-validation-8nov

--- src/app/pages/admin/create-role/create-role.component.ts
+++ src/app/pages/admin/create-role/create-role.component.ts
@@ -10,26 +10,61 @@ import { ApiService } from 'src/app/api/api.service';
 export class CreateRoleComponent implements OnInit {
 
   dataString;
-  roleId: null;
-  roleName: null;
-  roleGroup: null;  
+  roleId;
+  roleName;
+  roleGroup;  
+
+  errors;
+  error0;
+  error1;
 
   constructor(private apiService: ApiService, private router:Router) { }
   ngOnInit(): void {
+
     
   }
 
   createRole(form){
+
+
+    if(form.value.roleName ==''){
+      this.error0 ='true'; 
+      this.errors='true';
+     
+    }
+    else{
+      this.error0 ='';
+      this.errors='false';
+     
+    }
+
+      if(form.value.roleName ==''){
+      this.error1 ='true'; 
+      this.errors='true';
+     
+    }
+    else{
+      this.error1 ='';
+      this.errors='false';
+     
+    }
+
+    if (this.errors != 'true'){
 
     this.dataString=`{"roleId":"${form.value.roleId}", "roleName":"${form.value.roleName}", "roleGroup":"${form.value.roleGroup}"}`;
     
     console.log(this.dataString);
     this.apiService.postAPI('dev/addroles', this.dataString).subscribe((data)=>{
       console.log(data);
-      this.router.navigate(['/admin/list-roles']);
+      // this.router.navigate(['/admin/list-roles']);
 
     })
   }
-  
+
+  else{
+    console.log('fill the form')
+  }
+
+  }
 
 }
