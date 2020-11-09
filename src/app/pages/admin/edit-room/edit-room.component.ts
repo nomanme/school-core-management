a3c7da03c0fdb8c4a01e2d38bb7960c918f28efc
@Eva021 <farzana.eva021@gmail.com>
2020-11-09 17:01:31 +0600

    eva

--- src/app/pages/admin/edit-room/edit-room.component.ts
+++ src/app/pages/admin/edit-room/edit-room.component.ts
@@ -1,39 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/api/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api/api.service';
 
 @Component({
   selector: 'app-edit-room',
   templateUrl: './edit-room.component.html',
-  styleUrls: ['./edit-room.component.css']
+  styleUrls: ['./edit-room.component.css'],
 })
 export class EditRoomComponent implements OnInit {
-
   venueroomId;
   venueroom;
   venueId;
 
-  constructor(private apiService: ApiService, private router:Router, private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.venueroomId= this.activatedRoute.snapshot.paramMap.get('id');
-    this.apiService.getAPI(`dev/getvenueroom?venueroomId=${this.venueroomId}`).subscribe((data)=>{
-      console.log(data);
-      this.venueroom=data[0];
-      console.log(this.venueroom)
-    })
+    this.venueroomId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.apiService
+      .getAPI(`dev/getvenueroom?venueroomId=${this.venueroomId}`)
+      .subscribe((data) => {
+        console.log(data);
+        this.venueroom = data[0];
+        console.log(this.venueroom);
+      });
   }
-  
-  editRoom(form){
 
+  editRoom(form) {
     console.log(form.value);
-    let datajson=`{"venueroomId":"1","roomName":"${form.value.roomName}", "roomNumber":"${form.value.roomNumber}", "venueId":"${form.value.venueId}"}`;
+    let datajson = `{"venueroomId":"1","roomName":"${form.value.roomName}", "roomNumber":"${form.value.roomNumber}", "venueId":"${form.value.venueId}"}`;
     console.log(datajson);
-    this.apiService.postAPI(`dev/editvenueroom?venueroomId=${this.venueroomId}`, datajson).subscribe((data)=>{
-      console.log(data);
-      this.router.navigate(['/admin/list-venueroom']);
-    })
-
-}
-
+    this.apiService
+      .postAPI(`dev/editvenueroom?venueroomId=${this.venueroomId}`, datajson)
+      .subscribe((data) => {
+        console.log(data);
+        this.router.navigate(['/admin/list-venueroom']);
+      });
+  }
 }
--- src/app/pages/admin/edit-venue/edit-venue.component.ts
+++ src/app/pages/admin/edit-venue/edit-venue.component.ts
@@ -1,42 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/api/api.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api/api.service';
 
 @Component({
   selector: 'app-edit-venue',
   templateUrl: './edit-venue.component.html',
-  styleUrls: ['./edit-venue.component.css']
+  styleUrls: ['./edit-venue.component.css'],
 })
 export class EditVenueComponent implements OnInit {
+  getstate;
+  getcountry;
+  gettrainingOrg;
 
   venueId;
   venue;
 
-  constructor(private apiService: ApiService, private router:Router, private activatedRoute:ActivatedRoute) { }
-
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.venueId= this.activatedRoute.snapshot.paramMap.get('id');
+    this.apiService.getAPI3('dev/getstate').subscribe((data) => {
+      this.getstate = data;
+    });
 
-// `dev/user?userId=${}`
+    this.apiService.getAPI3('dev/getcountry').subscribe((data) => {
+      this.getcountry = data;
+    });
 
-    this.apiService.getAPI(`dev/getvenue?venueId=${this.venueId}`).subscribe((data)=>{
-      console.log(data);
-      this.venue=data[0];
-      console.log(this.venue)
-    })
+    this.apiService.getAPI('dev/gettrainingOrg').subscribe((data) => {
+      this.gettrainingOrg = data;
+    });
+
+    this.venueId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.apiService
+      .getAPI(`dev/getvenue?venueId=${this.venueId}`)
+      .subscribe((data) => {
+        console.log(data);
+        this.venue = data[0];
+        console.log(this.venue);
+      });
   }
 
-  editVenue(form){
-
+  editVenue(form) {
     console.log(form.value);
-    let datajson=`{"venueId":"1","roomName":"${form.value.roomName}", "roomNumber":"${form.value.roomNumber}", "venueId":"${form.value.venueId}"}`;
+    let datajson = `{"venueId":"1","roomName":"${form.value.roomName}", "roomNumber":"${form.value.roomNumber}", "venueId":"${form.value.venueId}"}`;
     console.log(datajson);
-    this.apiService.postAPI(`dev/editvenue?venueId=${this.venueId}`, datajson).subscribe((data)=>{
-      console.log(data);
-      this.router.navigate(['/admin/list-venueroom']);
-    })
-
-}
-
+    this.apiService
+      .postAPI(`dev/editvenue?venueId=${this.venueId}`, datajson)
+      .subscribe((data) => {
+        console.log(data);
+        this.router.navigate(['/admin/list-venueroom']);
+      });
+  }
 }
