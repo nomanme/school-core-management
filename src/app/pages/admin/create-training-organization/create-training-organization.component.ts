6cf96ea6d26dedb0b9a029747cd9d284d199736e
Rude Code <abdullah.noman083@gmail.com>
2020-11-11 14:22:24 +0600

    form-create

--- src/app/pages/admin/create-training-organization/create-training-organization.component.ts
+++ src/app/pages/admin/create-training-organization/create-training-organization.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-training-organization',
   templateUrl: './create-training-organization.component.html',
-  styleUrls: ['./create-training-organization.component.css']
+  styleUrls: ['./create-training-organization.component.css'],
 })
 export class CreateTrainingOrganizationComponent implements OnInit {
+  trainingOrgId;
+  RTOCode;
+  CRICOSCode;
+  trainingOrgName;
+  address1;
+  suburb;
+  stateId;
+  postCode;
+  RTOType;
+  trainingOrgTypeCode;
+  contactPerson;
+  telephone;
+  ABN;
+  contactEmail;
+  ITEmail;
+  academicEmail;
+  accountsEmail;
+  trainingOrgURL;
+  typeOfIndustryCode;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
