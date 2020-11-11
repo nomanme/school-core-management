1f798c70ff50507992362f217f566eebc6a3c763
Rude Code <abdullah.noman083@gmail.com>
2020-11-11 17:32:17 +0600

    validation-error

--- src/app/pages/admin/create-studentenrolment/create-studentenrolment.component.ts
+++ src/app/pages/admin/create-studentenrolment/create-studentenrolment.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ApiService } from 'src/app/api/api.service'; 
+import { ApiService } from 'src/app/api/api.service';
 
 @Component({
   selector: 'app-create-studentenrolment',
   templateUrl: './create-studentenrolment.component.html',
-  styleUrls: ['./create-studentenrolment.component.css']
+  styleUrls: ['./create-studentenrolment.component.css'],
 })
 export class CreateStudentenrolmentComponent implements OnInit {
-  
-  // getstudentorigin; 
+  // getstudentorigin;
   getcourse;
   getagent;
   getcourseintakedate;
@@ -26,272 +25,268 @@ export class CreateStudentenrolmentComponent implements OnInit {
   dataString;
   getstudentorigin;
   studentEnrolmentId;
-  studentOriginId='';
-  courseId='';
-  agentId='';
-  courseIntakeDateId='';
-  applicationStatusId='';
-  deliveryModeId='';
-  fundingSourceNationalId='';
-  fundingSourceStateId='';
-  commencingProgramId='';
-  trainingContractid='';
-  reasonTakingCourseId='';
-  applyForRPL='';
-  TuitionFee='';
+  studentOriginId = '';
+  courseId = '';
+  agentId = '';
+  courseIntakeDateId = '';
+  applicationStatusId = '';
+  deliveryModeId = '';
+  fundingSourceNationalId = '';
+  fundingSourceStateId = '';
+  commencingProgramId = '';
+  trainingContractid = '';
+  reasonTakingCourseId = '';
+  applyForRPL = '';
+  TuitionFee = '';
   userId;
   outputD;
 
-  allerrors='';
+  allerrors = '';
 
-  error0='';
-  error1='';
-  error2='';
-  error3='';
-  error4='';
-  error5='';
-  error6='';
-  error7='';
-  error8='';
-  error9='';
-  error10='';
-  error11='';
-  error12='';
-  error13='';
-  errors='';
-  errorCodes:{studentOriginId:''}
+  error0 = '';
+  error1 = '';
+  error2 = '';
+  error3 = '';
+  error4 = '';
+  error5 = '';
+  error6 = '';
+  error7 = '';
+  error8 = '';
+  error9 = '';
+  error10 = '';
+  error11 = '';
+  error12 = '';
+  error13 = '';
+  errors = '';
+  errorCodes: { studentOriginId: '' };
 
-  constructor(private apiService: ApiService, private router:Router) { }
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit(): void {
-    this.outputD=this.apiService.getLocalStorage('studentId');
+    this.outputD = this.apiService.getLocalStorage('studentId');
     console.log(this.outputD);
 
-
-    this.apiService.getAPI3('dev/getapplicationstatus').subscribe((data)=>{
+    this.apiService.getAPI3('dev/getapplicationstatus').subscribe((data) => {
       // console.log(data);
-      this.getapplicationstatus=data;
-    })
+      this.getapplicationstatus = data;
+    });
 
-    this.apiService.getAPI3('dev/getdeliverymode').subscribe((data)=>{
+    this.apiService.getAPI3('dev/getdeliverymode').subscribe((data) => {
       // console.log(data);
-      this.getdeliverymode=data;
-    })
+      this.getdeliverymode = data;
+    });
 
-    this.apiService.getAPI3('dev/getfundingsourcenational').subscribe((data)=>{
+    this.apiService
+      .getAPI3('dev/getfundingsourcenational')
+      .subscribe((data) => {
+        // console.log(data);
+        this.getfundingsourcenational = data;
+      });
+
+    this.apiService.getAPI3('dev/getfundingsourcestate').subscribe((data) => {
       // console.log(data);
-      this.getfundingsourcenational=data;
-    })
+      this.getfundingsourcestate = data;
+    });
 
-    this.apiService.getAPI3('dev/getfundingsourcestate').subscribe((data)=>{
+    this.apiService.getAPI3('dev/getcommencingprogram').subscribe((data) => {
       // console.log(data);
-      this.getfundingsourcestate=data;
-    })
+      this.getcommencingprogram = data;
+    });
 
-    this.apiService.getAPI3('dev/getcommencingprogram').subscribe((data)=>{
+    this.apiService.getAPI3('dev/getreasontakingcourse').subscribe((data) => {
       // console.log(data);
-      this.getcommencingprogram=data;
-    })
+      this.getreasontakingcourse = data;
+    });
 
-    this.apiService.getAPI3('dev/getreasontakingcourse').subscribe((data)=>{
+    this.apiService.getAPI('dev/getcourse').subscribe((data) => {
       // console.log(data);
-      this.getreasontakingcourse=data;
-    })
+      this.getcourse = data;
+    });
 
-    this.apiService.getAPI('dev/getcourse').subscribe((data)=>{
+    this.apiService.getAPI2('dev/getcourseintakedate').subscribe((data) => {
       // console.log(data);
-      this.getcourse=data;
-    })
+      this.getcourseintakedate = data;
+    });
 
-    this.apiService.getAPI2('dev/getcourseintakedate').subscribe((data)=>{
+    this.apiService.getAPI3('dev/getagent').subscribe((data) => {
       // console.log(data);
-      this.getcourseintakedate=data;
-    })
+      this.getagent = data;
+    });
 
-    this.apiService.getAPI3('dev/getagent').subscribe((data)=>{
-      // console.log(data);
-      this.getagent=data;
-    })
-
-    this.apiService.getAPI3('dev/getstudentorigin').subscribe((data)=>{
+    this.apiService.getAPI3('dev/getstudentorigin').subscribe((data) => {
       console.log(data);
-      this.getstudentorigin=data;
-    })
+      this.getstudentorigin = data;
+    });
 
-    this.apiService.getAPI3('dev/gettrainingcontract').subscribe((data)=>{
+    this.apiService.getAPI3('dev/gettrainingcontract').subscribe((data) => {
       // console.log(data);
-      this.gettrainingcontract=data;
-    })
+      this.gettrainingcontract = data;
+    });
 
-    this.apiService.getAPI3('dev/getqualification').subscribe((data)=>{
-      this.getqualification=data;
-    })
-
-
+    this.apiService.getAPI3('dev/getqualification').subscribe((data) => {
+      this.getqualification = data;
+    });
   }
 
-  createStudentEnrolment(form){
-
-
-    if(form.value.allerrors ==''){
-      this.error0 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error0 ='';
-      this.errors='false';
+  createStudentEnrolment(form) {
+    if (form.value.allerrors == '') {
+      this.error0 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error0 = '';
+      this.errors = 'false';
     }
 
-    console.log(form.value.studentOriginId)
+    console.log(form.value.studentOriginId);
 
-   if(form.value.studentOriginId ==''){
-     this.error1 ='true'; 
-      this.errors='true';
-     
+    if (form.value.studentOriginId == '') {
+      this.error1 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error1 = '';
+      this.errors = 'false';
     }
-    else{
-      this.error1 ='';
-      this.errors='false';
-     
-    }
-     console.log(`1: ${this.errors}`);
-    
-    if(form.value.courseId ==''){
-      this.error2 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error2 ='';
-      this.errors='false';
+    console.log(`1: ${this.errors}`);
+
+    if (form.value.courseId == '') {
+      this.error2 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error2 = '';
+      this.errors = 'false';
     }
 
     console.log(`2: ${this.errors}`);
-      
-      if(form.value.courseIntakeDateId ==''){
-       this.error3 ='true'; 
-        this.errors='true';
-      }
-      else{
-        this.error3 ='';
-        this.errors='false';
-      }
+
+    if (form.value.courseIntakeDateId == '') {
+      this.error3 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error3 = '';
+      this.errors = 'false';
+    }
     console.log(`3: ${this.errors}`);
 
-      if(form.value.applicationStatusId ==''){
-       this.error4 ='true'; 
-        this.errors='true';
-      }
-      else{
-        this.error4 ='';
-        this.errors='false';
-      }
-
-    if(form.value.deliveryModeId ==''){
-      this.error5 ='true'; 
-        this.errors='true';
-      }
-      else{
-        this.error5 ='';
-        this.errors='false';
-      }
-
-
-    if(form.value.fundingSourceNationalId ==''){
-    this.error6 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error6 ='';
-      this.errors='false';
+    if (form.value.applicationStatusId == '') {
+      this.error4 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error4 = '';
+      this.errors = 'false';
     }
 
-
-    if(form.value.fundingSourceStateId ==''){
-    this.error7 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error7 ='';
-      this.errors='false';
+    if (form.value.deliveryModeId == '') {
+      this.error5 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error5 = '';
+      this.errors = 'false';
     }
 
-    if(form.value.commencingProgramId ==''){
-    this.error8 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error8 ='';
-      this.errors='false';
-    }
-
-    if(form.value.courseId ==''){
-      this.error9 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error9 ='';
-      this.errors='false';
+    if (form.value.fundingSourceNationalId == '') {
+      this.error6 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error6 = '';
+      this.errors = 'false';
     }
 
-    
-    if(form.value.courseId ==''){
-    this.error10 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error10 ='';
-      this.errors='false';
+    if (form.value.fundingSourceStateId == '') {
+      this.error7 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error7 = '';
+      this.errors = 'false';
     }
 
-  
-    if(form.value.courseId ==''){
-    this.error11 ='true'; 
-      this.errors='true';
-    }
-    else{
-      this.error11 ='';
-      this.errors='false';
+    if (form.value.commencingProgramId == '') {
+      this.error8 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error8 = '';
+      this.errors = 'false';
     }
 
-    
-  if(form.value.courseId ==''){
-  this.error12 ='true'; 
-  this.errors='true';
+    if (form.value.courseId == '') {
+      this.error9 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error9 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.courseId == '') {
+      this.error10 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error10 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.courseId == '') {
+      this.error11 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error11 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.courseId == '') {
+      this.error12 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error12 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.courseId == '') {
+      this.error13 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error13 = '';
+      this.errors = 'false';
+    }
+
+    console.log(`atend: ${this.errors}`);
+
+    if (this.errors != 'true') {
+      this.dataString = `{"userId":"1", "studentId":"${this.apiService.getLocalStorage(
+        'studentId'
+      )}", "courseId":"${form.value.courseId}", "agentId":"${
+        form.value.agentId
+      }", "courseIntakeDateId":"${
+        form.value.courseIntakeDateId
+      }", "applicationStatusId":"${
+        form.value.applicationStatusId
+      }",  "applicationStatusId":"${
+        form.value.applicationStatusId
+      }","studentOriginId":"${
+        form.value.studentOriginId
+      }", "fundingSourceNationalId":"${
+        form.value.fundingSourceNationalId
+      }", "fundingSourceStateId":"${
+        form.value.fundingSourceStateId
+      }", "commencingProgramId":"${
+        form.value.commencingProgramId
+      }", "trainingContractid":"${
+        form.value.trainingContractid
+      }", "deliveryModeId":"${
+        form.value.deliveryModeId
+      }", "reasonTakingCourseId":"${
+        form.value.reasonTakingCourseId
+      }", "applyForRPL":"${form.value.applyForRPL}", "TuitionFee":"${
+        form.value.TuitionFee
+      }"}`;
+
+      console.log(this.dataString);
+      this.apiService
+        .postAPI2('dev/addstudentenrolment', this.dataString)
+        .subscribe((data) => {
+          // console.log(data);
+          // this.router.navigate(['/admin/list-student-enrolment']);
+        });
+    } else {
+      console.log('submit the form');
+    }
   }
-  else{
-      this.error12 ='';
-      this.errors='false';
-    }
-
-        
-  if(form.value.courseId ==''){
-  this.error13 ='true'; 
-  this.errors='true';
-  }
-  else{
-      this.error13 ='';
-      this.errors='false';
-    }
-
-console.log(`atend: ${this.errors}`);
-  
-if (this.errors != 'true'){
-
-this.dataString=`{"userId":"1", "studentId":"${this.apiService.getLocalStorage('studentId')}", "courseId":"${form.value.courseId}", "agentId":"${form.value.agentId}", "courseIntakeDateId":"${form.value.courseIntakeDateId}", "applicationStatusId":"${form.value.applicationStatusId}",  "applicationStatusId":"${form.value.applicationStatusId}","studentOriginId":"${form.value.studentOriginId}", "fundingSourceNationalId":"${form.value.fundingSourceNationalId}", "fundingSourceStateId":"${form.value.fundingSourceStateId}", "commencingProgramId":"${form.value.commencingProgramId}", "trainingContractid":"${form.value.trainingContractid}", "deliveryModeId":"${form.value.deliveryModeId}", "reasonTakingCourseId":"${form.value.reasonTakingCourseId}", "applyForRPL":"${form.value.applyForRPL}", "TuitionFee":"${form.value.TuitionFee}"}`;
-
-
-    console.log(this.dataString);
-    this.apiService.postAPI2('dev/addstudentenrolment', this.dataString).subscribe((data)=>{
-      // console.log(data); 
-      // this.router.navigate(['/admin/list-student-enrolment']);
-
-    })
-  }
-
-  else{
-    console.log('submit the form')
-  }
-
-  } 
-
 }
--- src/app/pages/admin/create-unit/create-unit.component.ts
+++ src/app/pages/admin/create-unit/create-unit.component.ts
@@ -5,17 +5,14 @@ import { ApiService } from 'src/app/api/api.service';
 @Component({
   selector: 'app-create-unit',
   templateUrl: './create-unit.component.html',
-  styleUrls: ['./create-unit.component.css']
+  styleUrls: ['./create-unit.component.css'],
 })
 export class CreateUnitComponent implements OnInit {
-
   getfieldofeducation;
   getschooltype;
   getspecificfunding;
 
-
-
-  dataString
+  dataString;
   units;
 
   unitId;
@@ -30,47 +27,63 @@ export class CreateUnitComponent implements OnInit {
   schoolTypeId;
   specificFundingId;
 
-  // error0 = '';
-  // error1 = '';
-  // error2 = '';
-  // error3 = '';
-  // error4 = '';
-  // error5 = '';
+  allerrors = '';
 
-  // errors = '';
+  error0 = '';
+  error1 = '';
+  error2 = '';
+  error3 = '';
+  error4 = '';
+  error5 = '';
+  error6 = '';
+  error7 = '';
+  error8 = '';
+  error9 = '';
+  error10 = '';
+  error11 = '';
+  error12 = '';
+  error13 = '';
+  errors = '';
 
-  constructor(private apiService: ApiService, private router: Router) { }
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit(): void {
-
     this.apiService.getAPI3('dev/getfieldofeducation').subscribe((data) => {
       this.getfieldofeducation = data;
       console.log(data);
-    })
+    });
 
     this.apiService.getAPI3('dev/getschooltype').subscribe((data) => {
       this.getschooltype = data;
-    })
+    });
 
     this.apiService.getAPI3('dev/getspecificfunding').subscribe((data) => {
       this.getspecificfunding = data;
-    })
-
+    });
   }
 
   createUnit(form) {
+    if (form.value.unitCode == '') {
+      this.error0 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error0 = '';
+      this.errors = 'false';
+    }
 
-    this.dataString = `{"unitId":"1","unitCode":"${form.value.unitCode}", "vetUnitCode":"${form.value.vetUnitCode}", "unitName":"${form.value.unitName}", "deliveryMode":"${form.value.deliveryMode}", "scheduledNominalHours":"${form.value.scheduledNominalHours}", "tuitionFee":"${form.value.tuitionFee}", "fieldofEducationId":"${form.value.fieldofEducationId}","vetFlag":"${form.value.vetFlag}", "schoolTypeId":"${form.value.schoolTypeId}", "specificFundingId":"${form.value.specificFundingId}"}`;
+    if (this.errors != 'true') {
+      this.dataString = `{"unitId":"1","unitCode":"${form.value.unitCode}", "vetUnitCode":"${form.value.vetUnitCode}", "unitName":"${form.value.unitName}", "deliveryMode":"${form.value.deliveryMode}", "scheduledNominalHours":"${form.value.scheduledNominalHours}", "tuitionFee":"${form.value.tuitionFee}", "fieldofEducationId":"${form.value.fieldofEducationId}","vetFlag":"${form.value.vetFlag}", "schoolTypeId":"${form.value.schoolTypeId}", "specificFundingId":"${form.value.specificFundingId}"}`;
 
-    console.log(this.dataString);
+      console.log(this.dataString);
 
-    this.apiService.postAPI('dev/addunit', this.dataString).subscribe((data) => {
-      console.log(data);
-      // this.router.navigate(['/admin/unit-list']);
-    });
-
-
+      this.apiService
+        .postAPI('dev/addunit', this.dataString)
+        .subscribe((data) => {
+          console.log(data);
+          // this.router.navigate(['/admin/unit-list']);
+        });
+    } else {
+      console.log('submit the form');
+    }
   }
-
-
 }
