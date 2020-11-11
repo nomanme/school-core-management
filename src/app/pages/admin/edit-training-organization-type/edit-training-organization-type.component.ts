327f000d72427a903a82c43370115c3d96b6e724
Rude Code <abdullah.noman083@gmail.com>
2020-11-11 16:59:31 +0600

    done-edit-view

--- src/app/pages/admin/edit-training-organization-type/edit-training-organization-type.component.ts
+++ src/app/pages/admin/edit-training-organization-type/edit-training-organization-type.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit-training-organization-type',
   templateUrl: './edit-training-organization-type.component.html',
-  styleUrls: ['./edit-training-organization-type.component.css']
+  styleUrls: ['./edit-training-organization-type.component.css'],
 })
 export class EditTrainingOrganizationTypeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
