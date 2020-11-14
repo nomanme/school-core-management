bc2d33e125814d44d355b97026cbf15d26bb73ec
Rude Code <abdullah.noman083@gmail.com>
2020-11-14 12:23:22 +0600

    timetable-create-schedule-validation

--- src/app/pages/timetable/create-schedule/create-schedule.component.ts
+++ src/app/pages/timetable/create-schedule/create-schedule.component.ts
@@ -5,31 +5,75 @@ import { ApiService } from 'src/app/api/api.service';
 @Component({
   selector: 'app-create-schedule',
   templateUrl: './create-schedule.component.html',
-  styleUrls: ['./create-schedule.component.css']
+  styleUrls: ['./create-schedule.component.css'],
 })
 export class CreateScheduleComponent implements OnInit {
-
   schedules;
   dataString;
-  semisterId;
-  termId; 
-  weekId;
+  semisterId = '';
+  termId = '';
+  weekId = '';
   startDate;
-  constructor(private apiService: ApiService, private router:Router) { }
 
-  ngOnInit(): void {
- 
+  allerrors = '';
+
+  error0 = '';
+  error1 = '';
+  error2 = '';
+  error3 = '';
+  error4 = '';
+  error5 = '';
+
+  errors = '';
+
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  createSchedule(form) {
+    if (form.value.semisterId == '') {
+      this.error0 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error0 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.termId == '') {
+      this.error1 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error1 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.weekId == '') {
+      this.error2 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error2 = '';
+      this.errors = 'false';
+    }
+
+    // if (form.value.weekId == '') {
+    //   this.error0 = 'true';
+    //   this.errors = 'true';
+    // } else {
+    //     this.error0 = '';
+    //     this.errors = 'false';
+    // }
+
+    if (this.errors != 'true') {
+      this.dataString = `{"scheduleId":"1","semisterId":"${form.value.semisterId}", "termId":"${form.value.termId}", "weekId":"${form.value.weekId}","startDate":"${form.value.startDate}" }`;
+      console.log(this.dataString);
+      this.apiService
+        .postAPI('dev/addschedule', this.dataString)
+        .subscribe((data) => {
+          console.log(data);
+          this.router.navigate(['/timetable/list-schedule']);
+        });
+    } else {
+      ('There is a error');
+    }
   }
-
-  createSchedule(form){
-
-    this.dataString=`{"scheduleId":"1","semisterId":"${form.value.semisterId}", "termId":"${form.value.termId}", "weekId":"${form.value.weekId}","startDate":"${form.value.startDate}" }`;
-    console.log(this.dataString);
-    this.apiService.postAPI('dev/addschedule', this.dataString).subscribe((data)=>{
-      console.log(data);
-      this.router.navigate(['/timetable/list-schedule']);
-
-    })
-  }
-
 }
--- src/app/pages/timetable/edit-schedule/edit-schedule.component.ts
+++ src/app/pages/timetable/edit-schedule/edit-schedule.component.ts
@@ -5,41 +5,81 @@ import { ApiService } from 'src/app/api/api.service';
 @Component({
   selector: 'app-edit-schedule',
   templateUrl: './edit-schedule.component.html',
-  styleUrls: ['./edit-schedule.component.css']
+  styleUrls: ['./edit-schedule.component.css'],
 })
 export class EditScheduleComponent implements OnInit {
-
   scheduleId;
   schedule = {
-    semisterId: null,
-    termId: null,
-    weekId: null,
-    startDate: null,
+    semisterId: '',
+    termId: '',
+    weekId: '',
+    startDate: '',
   };
 
-  constructor(private apiService: ApiService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  error0 = '';
+  error1 = '';
+  error2 = '';
+  error3 = '';
+  error4 = '';
 
+  errors = '';
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.scheduleId = this.activatedRoute.snapshot.paramMap.get('id');
 
-    this.apiService.getAPI(`dev/getschedule?scheduleId=${this.scheduleId}`).subscribe((data) => {
-      console.log(data);
-      this.schedule = data[0];
-      console.log(this.schedule)
-    });
+    this.apiService
+      .getAPI(`dev/getschedule?scheduleId=${this.scheduleId}`)
+      .subscribe((data) => {
+        console.log(data);
+        this.schedule = data[0];
+        console.log(this.schedule);
+      });
   }
 
   editSchedule(form) {
+    if (form.value.semisterId == '') {
+      this.error0 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error0 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.termId == '') {
+      this.error1 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error1 = '';
+      this.errors = 'false';
+    }
+
+    if (form.value.weekId == '') {
+      this.error2 = 'true';
+      this.errors = 'true';
+    } else {
+      this.error2 = '';
+      this.errors = 'false';
+    }
 
     console.log(form.value);
-    let datajson = `{"scheduleId":"1","semisterId":"${form.value.semisterId}", "termId":"${form.value.termId}", "weekId":"${form.value.weekId}","startDate":"${form.value.startDate}" }`;
-    console.log(datajson);
-    this.apiService.postAPI(`dev/editschedule?scheduleId=${this.scheduleId}`, datajson).subscribe((data) => {
-      console.log(data);
-      this.router.navigate(['/timetable/list-schedule']);
-    });
 
+    if (this.errors != 'true') {
+      let datajson = `{"scheduleId":"1","semisterId":"${form.value.semisterId}", "termId":"${form.value.termId}", "weekId":"${form.value.weekId}","startDate":"${form.value.startDate}" }`;
+      console.log(datajson);
+      this.apiService
+        .postAPI(`dev/editschedule?scheduleId=${this.scheduleId}`, datajson)
+        .subscribe((data) => {
+          console.log(data);
+          this.router.navigate(['/timetable/list-schedule']);
+        });
+    } else {
+      ('there is error');
+    }
   }
-
 }
