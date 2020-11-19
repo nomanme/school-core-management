b5a22471aee9b4a92ef715715c4287b91112853b
@Eva021 <farzana.eva021@gmail.com>
2020-11-19 18:59:39 +0600

    Ev19nov

--- src/app/pages/admin/studentcreate/studentcreate.component.ts
+++ src/app/pages/admin/studentcreate/studentcreate.component.ts
@@ -32,6 +32,7 @@ export class StudentcreateComponent implements OnInit {
 
   students;
   dataString;
+  dataString3;
 
   PriorEducationalAchievementFlag = '';
   altEmail = '';
@@ -78,9 +79,25 @@ export class StudentcreateComponent implements OnInit {
   visaExpdate = '';
   visaNo = '';
   visaStatusId = "None";
+
   dataString2;
-  outputD;
   outputD2;
+  postalbuildingName = '';
+  postalflatUnitDetails = '';
+  postalpostCode = '';
+  postalstreetName = '';
+  postalstreetNumber = '';
+  postalstateId = '';
+  postalsuburb = '';
+  postalpobox = '';
+
+
+
+
+
+
+  outputD;
+
 
   errors = '';
   allerrors = '';
@@ -394,30 +411,44 @@ export class StudentcreateComponent implements OnInit {
 
     if (this.errors != 'true') {
       this.dataString = `{
-      "userId":"1", "clientId": ${this.getclientid}, "PriorEducationalAchievementFlag":"${form.value.PriorEducationalAchievementFlag}", "altEmail":"${form.value.altEmail}", "australianPr":"${form.value.australianPr}", "birthcountryId": "${form.value.birthcountryId}", "buildingName": "${form.value.buildingName}",  "completedSchoolLevelId": "${form.value.completedSchoolLevelId}", "differentPostalAddress": "${form.value.differentPostalAddress}", "disability": "${form.value.disability}", "dob": "${form.value.dob}", "email":"${form.value.email}", "employmentStatusId": "${form.value.employmentStatusId}", "englishSpeakingStatusId": "${form.value.englishSpeakingStatusId}", "firstName": "${form.value.firstName}", "flatUnitDetails": "${form.value.flatUnitDetails}", "gender": "${form.value.gender}", "homeLanguageId": "${form.value.homeLanguageId}", "indigenousStatusId": "${form.value.indigenousStatusId}", "lastName": "${form.value.lastName}", "middleName": "${form.value.middleName}", "mobile": "${form.value.mobile}", "nationalityId": "${form.value.nationalityId}", "passportExpdate": "${form.value.passportExpdate}", "passportNo": "${form.value.passportNo}", "postCode": "${form.value.postCode}", "schoolTypeId": "${form.value.schoolTypeId}", "signatoryText": "${form.value.signatoryText}", "stateId": "${form.value.stateId}", "statisticalAreaLevel1Id": "${form.value.statisticalAreaLevel1Id}", "statisticalAreaLevel2Id": "${form.value.statisticalAreaLevel2Id}", "stillInSecSchool": "${form.value.stillInSecSchool}", "streetName": "${form.value.streetName}", "streetNumber": "${form.value.streetNumber}", "suburb": "${form.value.suburb}", "surveyContactStatusId": "${form.value.surveyContactStatusId}", "telHome": "${form.value.telHome}", "telWork": "${form.value.telWork}", "title": "${form.value.title}", "usiNo": "${form.value.usiNo}", "visaExpdate": "${form.value.visaExpdate}", "visaNo": "${form.value.visaNo}", "visaStatusId": "${form.value.visaStatusId}"
-    }`;
-
+      "userId":"1", "clientId": ${this.getclientid}, "PriorEducationalAchievementFlag":"${form.value.PriorEducationalAchievementFlag}", "altEmail":"${form.value.altEmail}", "australianPr":"${form.value.australianPr}", "birthcountryId": "${form.value.birthcountryId}", "buildingName": "${form.value.buildingName}",  "completedSchoolLevelId": "${form.value.completedSchoolLevelId}", "differentPostalAddress": "${form.value.differentPostalAddress}", "disability": "${form.value.disability}", "dob": "${form.value.dob}", "email":"${form.value.email}", "employmentStatusId": "${form.value.employmentStatusId}", "englishSpeakingStatusId": "${form.value.englishSpeakingStatusId}", "firstName": "${form.value.firstName}", "flatUnitDetails": "${form.value.flatUnitDetails}", "gender": "${form.value.gender}", "homeLanguageId": "${form.value.homeLanguageId}", "indigenousStatusId": "${form.value.indigenousStatusId}", "lastName": "${form.value.lastName}", "middleName": "${form.value.middleName}", "mobile": "${form.value.mobile}", "nationalityId": "${form.value.nationalityId}", "passportExpdate": "${form.value.passportExpdate}", "passportNo": "${form.value.passportNo}", "postCode": "${form.value.postCode}", "schoolTypeId": "${form.value.schoolTypeId}", "signatoryText": "${form.value.signatoryText}", "stateId": "${form.value.stateId}", "statisticalAreaLevel1Id": "${form.value.statisticalAreaLevel1Id}", "statisticalAreaLevel2Id": "${form.value.statisticalAreaLevel2Id}", "stillInSecSchool": "${form.value.stillInSecSchool}", "streetName": "${form.value.streetName}", "streetNumber": "${form.value.streetNumber}", "suburb": "${form.value.suburb}", "surveyContactStatusId": "${form.value.surveyContactStatusId}", "telHome": "${form.value.telHome}", "telWork": "${form.value.telWork}", "title": "${form.value.title}", "usiNo": "${form.value.usiNo}", "visaExpdate": "${form.value.visaExpdate}", "visaNo": "${form.value.visaNo}", "visaStatusId": ${form.value.visaStatusId}}`;
+      console.log(this.dataString);
       // this.apiService.postAPI('dev/addstudent', this.dataString).subscribe((data) => {
       //   console.log(data);
       //   this.router.navigate(['/admin/unit-list']);
 
 
-      // this.dataString2=`{"clientId":"TEST190007","title":null,"gender":"F","firstName":"firstName Test 9999","middleName":null,"lastName":"test test test","dob":"2019-01-01","buildingName":null,"flatUnitDetails":null,"streetNumber":2,"streetName":"streetName","suburb":"suburb","postCode":2066,"stateId":1,"telHome":null,"telWork":null,"mobile":null,"email":"email1@email.com","altEmail":null,"differentPostalAddress":0,"birthcountryId":1,"nationalityId":null,"homeLanguageId":1,"englishSpeakingStatusId":1,"australianPr":null,"indigenousStatusId":1,"employmentStatusId":1,"stillInSecSchool":null,"schoolTypeId":1,"completedSchoolLevelId":1,"PriorEducationalAchievementFlag":"Y","disability":"N","passportNo":null,"passportExpdate":null,"visaStatusId":null,"visaNo":null,"visaExpdate":null,"usiNo":null,"signatoryText":null,"surveyContactStatusId":1,"statisticalAreaLevel1Id":null,"statisticalAreaLevel2Id":null,"userId":1 }`;
-      // console.log(this.dataString);
 
       this.apiService
-        .postAPI('dev/addstudent', this.dataString)
-        .subscribe((data) => {
+        .postAPI('dev/addstudent', this.dataString).subscribe((data) => {
           console.log(data);
           // this.outputD = data;
-          // this.outputD = JSON.parse(this.outputD['msg']);
-          // this.outputD = this.outputD[0].studentId;
+          this.outputD = JSON.parse(data['msg']);
+          this.outputD = this.outputD[0].studentId;
+          // this.outputD = data['msg'][0];
           // console.log(data);
 
+          console.log(this.outputD);
+
+
+          this.dataString2 = `{"studentId":"${this.outputD}", "buildingName":"${form.value.postalbuildingName}", "flatUnitDetails":"${form.value.postalflatUnitDetails}", "streetName":"${form.value.postalstreetName}", "streetNumber": "${form.value.postalstreetNumber}", "suburb": "${form.value.postalsuburb}", "stateId": "${form.value.postalstateId}", "postCode": "${form.value.postalpostCode}", "pobox": "${form.value.postalpobox}"}`;
+
+          console.log(this.dataString2);
           // console.log(this.outputD);
+          this.apiService.setLocalStorage('studentId', this.outputD);
+
+          this.apiService.postAPI2('dev/addstudentpostaldetails', this.dataString2).subscribe((data2) => {
+            // this.outputD2=JSON.parse(data2['msg'])[0]['studentPostalDetailsId'];
+            // console.log(data2);
+            // this.outputD2=data2;
+            // this.outputD2=JSON.parse(this.outputD2['msg']);
+            // this.outputD2=this.outputD2[0].studentPostalDetailsId;
+
+            this.router.navigate(['/admin/create-student-enrolment']);
+          });
 
 
-          // this.dataString2 = `{"userId":"1", "studentId":"${this.outputD}",  "buildingName":"${form.value.buildingName}", "flatUnitDetails":"${form.value.flatUnitDetails}", "streetName":"${form.value.streetName}", "streetNumber": "${form.value.streetNumber}", "suburb": "${form.value.suburb}", "stateId": "${form.value.stateId}", "postCode": "${form.value.postCode}", "pobox": "${form.value.pobox}", "stateId": "${form.value.stateId}"}`;
+          // this.dataString3 = `{"userId":"1", "studentId":"${this.outputD}",  "buildingName":"${form.value.buildingName}", "flatUnitDetails":"${form.value.flatUnitDetails}", "streetName":"${form.value.streetName}", "streetNumber": "${form.value.streetNumber}", "suburb": "${form.value.suburb}", "stateId": "${form.value.stateId}", "postCode": "${form.value.postCode}", "pobox": "${form.value.pobox}", "stateId": "${form.value.stateId}"}`;
           // console.log(this.dataString2);
           // // console.log(this.outputD);
           // this.apiService.setLocalStorage('studentId', this.outputD);
@@ -429,7 +460,7 @@ export class StudentcreateComponent implements OnInit {
           //   // this.outputD2=JSON.parse(this.outputD2['msg']);
           //   // this.outputD2=this.outputD2[0].studentPostalDetailsId;
 
-          //   // this.router.navigate(['/admin/create-student-enrolment']);
+          //   this.router.navigate(['/admin/create-student-enrolment']);
           // });
         });
     }
